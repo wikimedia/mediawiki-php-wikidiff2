44ab0aab9ee7389d7133f60a984efc4c540c8ac9
Ori Livneh <ori.livneh@gmail.com>
2019-05-06 17:10:31 -0400

    Add more benchmarks
    Add a benchmark script that times diff generation for 50 changes,
    sampled from Special:RecentChanges of several Wikipedias (en, hi, ru,
    th, and zh).
    
    There are a number of reasons why this workload may not be
    representative, but it's still a better test of "typical" performance
    than chinese-reverse-bench.php, which looks at a worst-case example.
    
    Change-Id: I2db5e18d1fe8c9d7ae91a68464ba0a4461d9a91d

---
+++ benchmarks/bench.php
@@ -0,0 +1,31 @@
+<?php
+// Benchmark script for Wikidiff2. It times diffing of 50 changes sampled
+// from Special:RecentChanges of several Wikipedias (en, hi, ru, th, and zh).
+$num_loops = 50;
+$data = json_decode( gzdecode( file_get_contents( __DIR__ . '/revs.json.gz' ) ), true );
+
+echo "loops: " . $num_loops . "\n";
+foreach ( $data as $lang => $revs ) {
+	$minTimeMs = INF;
+	$maxTimeMs = 0;
+	$times = [];
+	for ( $i = 1; $i <= $num_loops; $i++ ) {
+		$start = microtime( true );
+		foreach ( $revs as list( $a, $b ) ) {
+			$diff = wikidiff2_do_diff( $a, $b, 2 );
+			$diff = wikidiff2_do_diff( $b, $a, 2 );
+		}
+		$end = microtime( true );
+		$timeMs = ( $end - $start ) * 1000;
+		if ( $timeMs < $minTimeMs ) {
+			$minTimeMs = $timeMs;
+		}
+		if ( $timeMs > $maxTimeMs ) {
+			$maxTimeMs = $timeMs;
+		}
+		$times[] = $timeMs;
+	}
+	$avgTimeMs = array_sum( $times ) / count( $times );
+	printf( "[%s] best: %6.2fms   worst: %6.2fms   avg: %6.2fms\n",
+		$lang, $minTimeMs, $maxTimeMs, $avgTimeMs );
+}
