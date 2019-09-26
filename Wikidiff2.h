9fc2c0cbabcc0172507988e0cf2510d70e0ed4b1
Tim Starling <tstarling@wikimedia.org>
2019-09-27 09:46:59 +1000

    Update version to 1.9.0
    Change-Id: I7d8d23e4cc13caa1712a2d5a62892c174bcc3cbb

--- Wikidiff2.h
+++ Wikidiff2.h
@@ -16,7 +16,7 @@
 #include <set>
 #include <memory>
 
-#define WIKIDIFF2_VERSION_STRING "1.8.2"
+#define WIKIDIFF2_VERSION_STRING "1.9.0"
 // uncomment this for inline HTML debug output related to moved lines
 //#define DEBUG_MOVED_LINES
 
