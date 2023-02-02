fc001969afea5ec4b974559eb690c35ce133854a
Tim Starling <tstarling@wikimedia.org>
2023-02-02 15:07:28 +1100

    bench.php improvements
    * Reorder the nested loops so that we test each revision separately and
      report min/max for revisions.
    * Report the worst revision.
    * Use getopt() to specify iterations and max split size.
    
    Change-Id: I32f0e6b4b6126142b0d4d97d33a0485777cbe527

--- tools/benchmarks/bench.php
+++ tools/benchmarks/bench.php
@@ -1,19 +1,27 @@
 <?php
 // Benchmark script for Wikidiff2. It times diffing of 50 changes sampled
 // from Special:RecentChanges of several Wikipedias (en, hi, ru, th, and zh).
-$num_loops = 50;
+
+$opt = getopt( '', [ 'count:', 'split:' ] );
+
+$num_loops = $opt['count'] ?? 50;
 $data = json_decode( gzdecode( file_get_contents( __DIR__ . '/../data/revs.json.gz' ) ), true );
+
+$options = [
+	'maxSplitSize' => $opt['split'] ?? 1,
+];
 
 echo "loops: " . $num_loops . "\n";
 foreach ( $data as $lang => $revs ) {
 	$minTimeMs = INF;
 	$maxTimeMs = 0;
+	$worstCase = -1;
 	$times = [];
-	for ( $i = 1; $i <= $num_loops; $i++ ) {
+	foreach ( $revs as $revIndex => list( $a, $b ) ) {
 		$start = microtime( true );
-		foreach ( $revs as list( $a, $b ) ) {
-			$diff = wikidiff2_do_diff( $a, $b, 2 );
-			$diff = wikidiff2_do_diff( $b, $a, 2 );
+		for ( $i = 1; $i <= $num_loops; $i++ ) {
+			$diff = wikidiff2_multi_format_diff( $a, $b, $options );
+			$diff = wikidiff2_multi_format_diff( $b, $a, $options );
 		}
 		$end = microtime( true );
 		$timeMs = ( $end - $start ) * 1000;
@@ -22,10 +30,11 @@ foreach ( $data as $lang => $revs ) {
 		}
 		if ( $timeMs > $maxTimeMs ) {
 			$maxTimeMs = $timeMs;
+			$worstCase = $revIndex;
 		}
 		$times[] = $timeMs;
 	}
 	$avgTimeMs = array_sum( $times ) / count( $times );
-	printf( "[%s] best: %6.2fms   worst: %6.2fms   avg: %6.2fms\n",
+	printf( "[%s] best: %6.2fms   worst: %6.2fms (rev #$worstCase)   avg: %6.2fms\n",
 		$lang, $minTimeMs, $maxTimeMs, $avgTimeMs );
 }
