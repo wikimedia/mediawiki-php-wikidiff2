6b7288a6794431d62af9eea9327b5e39ec3c248e
Taavi Väänänen <hi@taavi.wtf>
2026-08-05 00:08:53 +0300

    Remove obsolete copyright header
    The header was added by ext_skel from an old version of PHP, but that
    script no longer adds a license header by default, it is only
    recommended for in-tree extensions. So presumably the template can be
    used without restrictions. The subsequent modifications are licensed
    under GPLv2 or newer, per I86f2c49376ecb1e3e3ead56a2cd9ccc1b25087ea.
    
    Bug: T196132
    Change-Id: Ifc8f508d3cf3db4a8e5733ca5f3ee66e4c8ee7e8

--- src/php_wikidiff2.h
+++ src/php_wikidiff2.h
@@ -1,23 +1,3 @@
-/*
-  +----------------------------------------------------------------------+
-  | PHP Version 5                                                        |
-  +----------------------------------------------------------------------+
-  | Copyright (c) 1997-2008 The PHP Group                                |
-  +----------------------------------------------------------------------+
-  | This source file is subject to version 3.01 of the PHP license,      |
-  | that is bundled with this package in the file LICENSE, and is        |
-  | available through the world-wide-web at the following url:           |
-  | http://www.php.net/license/3_01.txt                                  |
-  | If you did not receive a copy of the PHP license and are unable to   |
-  | obtain it through the world-wide-web, please send a note to          |
-  | license@php.net so we can mail you a copy immediately.               |
-  +----------------------------------------------------------------------+
-  | Author:                                                              |
-  +----------------------------------------------------------------------+
-*/
-
-/* $Id: header,v 1.16.2.1.2.1.2.1 2008/02/07 19:39:50 iliaa Exp $ */
-
 #ifndef PHP_WIKIDIFF2_H
 #define PHP_WIKIDIFF2_H
 
